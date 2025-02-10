5dd4672e62a29094fdd3a7c49320a4d54ca13524
Matilda109 <gagosaidit@gmail.com>
2025-02-10 15:25:10 +0000

    init

--- obj/Release/net8.0/FunctionForce.AssemblyInfo.cs
+++ obj/Release/net8.0/FunctionForce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FunctionForce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f577aaa92d5dff606fdc97d72a1ec8d9c1b32726")]
 [assembly: System.Reflection.AssemblyProductAttribute("FunctionForce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FunctionForce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
